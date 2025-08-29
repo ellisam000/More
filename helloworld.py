4a869a69c805625b69190f62d0b43275b1bd0ea9
ELLISAM000 <ellisam000@warren.k12.in.us>
2025-08-29 08:29:44 -0400

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+print("Hello, world!")
+
+print("My name is Amya.")
+print("i like food.")
+
+
+school_name = "warren Central High School"
+math_grade = 100
+science_grade = 90
+english_grade = 80
+all_passing = True
+
+print("school:", school_name)
+print("math grade:", math_grade)
+print("science grade:", science_grade)
+print("english grade:", english_grade)
