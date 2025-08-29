ac32f7114de08bbfa6949f44a020504876df2670
Amya Ellis <ellisam000@warren.k12.in.us>
2025-08-29 08:35:39 -0400

    updates

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,33 @@
-print("Hello, world!")
+# Student bio card
 
-print("My name is Amya.")
-print("i like food.")
+# TODO: Create a variable named name and store a string literal  in it representing someone's name
+name = "Charlie"
+# TODO: Create a variable named age and store an integer literal in it representing someone's age in year.
+age = 15
+# TODO: Create a variable named favorite_food and store a string literal in it representing someone's favorite food.
+favorite_food = "Pizza"
+# TODO: repeat for the following variables:  dream_job, hobby
+dream_job = "Programmer"
+hobby = "Fishing"
+
+# Print using commas
+
+# TODO: print out the name variable using commas in the print function.  Output should be:  Name: (value stored in name)
+print("Name:", name)
+# TODO: print out the age variable using commas in the print function.  Output should be:  Age: (value stored in age)
+
+# TODO: print out the favorite_food variable using commas in the print function.  Output should be:  Favorite Food: (value stored in favorite_food)
 
 
-school_name = "warren Central High School"
-math_grade = 100
-science_grade = 90
-english_grade = 80
-all_passing = True
+# Print using concatenation
 
-print("school:", school_name)
-print("math grade:", math_grade)
-print("science grade:", science_grade)
-print("english grade:", english_grade)
+# TODO: print out the dream_job variable using concatenation in the print function.  Output should be:  Dream Job: (value stored in dream_job)
+print("Dream Job: " + dream_job)
+# TODO: print out the hobby variable using concatenation in the print function.  Output should be:  Hobby: (value stored in hobby)
+
+
+# Print using f-string
+
+# TODO: using the f-string format in print so that the output is as follows.  (Value in Name) is (Value in age) years old and want to be a (Value in dream_job).
+print(f"{name} is {age} year old and wants to be a {dream_job}")
+# TODO: using the f-string format in print so that the output is as follows.  They love eating (Value in favorite_food) and enjoy (Value in hobby).
